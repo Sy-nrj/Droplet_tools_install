cd910be4f8d34ab0397df50c5185f992c5014338
Sy-nrj <lsmkark99@gmail.com>
2025-06-17 23:56:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ go install github.com/lc/subjs@latest
 go install github.com/tomnomnom/anew@latest
 go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 go install github.com/rverton/webanalyze/cmd/webanalyze@latest
-
+go install github.com/BishopFox/jsluice/cmd/jsluice@latest
 
 
 sudo apt install cewl
